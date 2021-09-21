ce4e10262e29458731ecb8ad081bec8d36fc04dd
Yoga Putra <yogapupratama@gmail.com>
2021-09-21 10:35:03 +0700

    add Off-Exchange Trading

---
+++ src/fetch/OffExchangeTrading.js
@@ -0,0 +1,45 @@
+function OffExchangeTrading(loadElement, elements) {
+	const data = {};
+	const dataListTemp = [];
+
+	// Find title
+	elements.find('div.data__details_metrics > div.data__details_metrics_item > div.data__details_metrics_item_title').map((_, innerChart) => {
+		const dataInnerChart = loadElement(innerChart);
+		const dataInnerChartTitle = dataInnerChart.find('h5').text();
+
+		if (dataInnerChartTitle.includes('Off-Exchange')) {
+			data.title = dataInnerChartTitle;
+		}
+
+	}).get();
+
+	// Find description
+	elements.find('div.data__details_metrics > div.data__details_metrics_item').map((_, innerChart) => {
+		const dataInnerChart = loadElement(innerChart);
+		const dataInnerChartDescription = dataInnerChart.find('p').text();
+
+		if (dataInnerChartDescription.includes('off-exchange')) {
+			data.description = dataInnerChartDescription;
+		}
+
+	}).get();
+
+	// Find data
+	elements.find('div.data__details_metrics_item > div.data__details_metrics_item_list > div.data__details_metrics_item_list_inner > div.data__details_metrics_item_list_field').map((_, innerChart) => { 
+		const dataDict = {};
+		
+		const dataInnerChart = loadElement(innerChart);
+		const dataKey = dataInnerChart.find('div.data__details_metrics_item_list_field_left > div.data__details_metrics_item_list_text > i').text();
+		const dataValue = dataInnerChart.find('div.data__details_metrics_item_list_field_right').text();
+
+		dataDict[dataKey] = dataValue;
+		dataListTemp.push(dataDict);
+
+	}).get();
+
+	data.listData = dataListTemp;
+
+	return data;
+}
+
+export default OffExchangeTrading;
--- src/utils/FetchCore.js
+++ src/utils/FetchCore.js
@@ -1,7 +1,8 @@
 import cheerio from 'cheerio';
 import HtmlResponse from './BaseSite.js';
-import WallStreetBets from '../fetch/WallStreetBets.js';
 import Twitter from '../fetch/Twitter.js';
+import WallStreetBets from '../fetch/WallStreetBets.js';
+import OffExchangeTrading from '../fetch/OffExchangeTrading.js';
 
 async function FetchCore(code) {
 	// Load html source from website.
@@ -13,10 +14,11 @@ async function FetchCore(code) {
 	const dataDetails = loadElement('div.data__details').map((_, elemet) => {
 		const dataElements = loadElement(elemet);
 		const dataTitle = dataElements.find('h3.data__details_title').text();
+		const offExchangeTrading = OffExchangeTrading(loadElement, dataElements);
 		const wallStreetBets = WallStreetBets(loadElement, dataElements);
 		const twitter = Twitter(loadElement, dataElements);
 
-		return { dataTitle, twitter, wallStreetBets };
+		return { dataTitle, offExchangeTrading, wallStreetBets, twitter };
 	}).get();
 
 	return dataDetails;
